3f477e84b8aebb3929443e88ac8be8ac263004c1
Pablo JT <pablotulian10@gmail.com>
2019-08-02 13:56:57 -0300

    Revert "JarToWar"
    This reverts commit 4d12c34f0de73e057577826ce717d8d28685ab77.

--- src/main/java/com/pablo/tulian/PruebasRestDbahr1Application.java
+++ src/main/java/com/pablo/tulian/PruebasRestDbahr1Application.java
@@ -2,17 +2,10 @@ package com.pablo.tulian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PruebasRestDbahr1Application extends SpringBootServletInitializer {
+public class PruebasRestDbahr1Application {
 
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(PruebasRestDbahr1Application.class);
-    }
-	
 	public static void main(String[] args) {
 		SpringApplication.run(PruebasRestDbahr1Application.class, args);
 	}
